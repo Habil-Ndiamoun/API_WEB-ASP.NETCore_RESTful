81ac12f6e1fdc6928a7dc88648eb0abd09d24715
Habil-Ndiamoun <85562269+Habil-Ndiamoun@users.noreply.github.com>
2023-12-16 13:33:05 -0500

    Création de la classe de context MunicipaliteContext

---
+++ Gestion_Municipalites/Data/MunicipaliteContext.cs
@@ -0,0 +1,16 @@
+﻿using Gestion_Municipalites.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestion_Municipalites.Data
+{
+    public class MunicipaliteContext : DbContext
+    {
+        public MunicipaliteContext(DbContextOptions<MunicipaliteContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Municipalite> Municipalites { get; set; }
+        public DbSet<Election> Elections { get; set; }
+    }
+}
