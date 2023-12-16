c4afe0c7b0079014344f3a425e7b80b1ea484c1f
Habil-Ndiamoun <85562269+Habil-Ndiamoun@users.noreply.github.com>
2023-12-16 13:25:42 -0500

    Création des classes pour représenter les entités Municipalite et Election

---
+++ Gestion_Municipalites/Models/Election.cs
@@ -0,0 +1,9 @@
+﻿namespace Gestion_Municipalites.Models
+{
+    public class Election
+    {
+        public int ElectionId { get; set; }
+        public int CodeGeographique { get; set; }
+        public DateTime DateElections { get; set; }
+    }
+}
---
+++ Gestion_Municipalites/Models/Municipalite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gestion_Municipalites.Models
+{
+    public class Municipalite
+    {
+        public int MunicipaliteId { get; set; }
+
+        [MaxLength(100)]
+        public string? NomMunicipalite { get; set;}
+
+        [MaxLength(100)]
+        public string?  AdresseCourriel { get; set; }
+        public string? AdresseWeb { get; set; }
+        public DateTime? DateProchaineElection { get; set; }
+        public bool Actif { get; set; }
+        public ICollection<Election>? Elections { get; set; }
+    }
+}
